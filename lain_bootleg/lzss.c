ccffd5e26d638968fecb1ff02fdcdcb5f4621747
elliotcraft79 <16109561+elliotcraft79@users.noreply.github.com>
2021-09-06 20:55:29 +0930

    make lzss.c actually work

--- lain_bootleg/lzss.c
+++ lain_bootleg/lzss.c
@@ -1,6 +1,6 @@
 /* LZSS decompression code from lain bootleg, modified output from ghidra so
- * there are basically no actual variable names because I can't be bothered,
- * so good luck trying to actually read this code! */
+ * some variables don't have meaningful names, but thanks to zerno, functions and most variables
+ * do have proper names. */
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -8,29 +8,29 @@
 #include <string.h>
 
 char *LZSS_compressed_data;
-char DAT_00412fbc;
-int DAT_00412fbd;
+unsigned char current_compressed_byte;
+int LZSS_bit_counter;
 char *LZSS_Output_Memory;
 int DAT_00412fe0;
-char DAT_0040d098[8] = {0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1};
+char BIT_MASKS[8] = {0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1};
 unsigned int DAT_00412fc8;
 unsigned int DAT_00412fcc;
 unsigned int DAT_00412fd0;
-unsigned int DAT_00412fc4;
+unsigned int circular_buffer_current;
 unsigned int DAT_00412fd4;
 unsigned int DAT_00412fdc;
-char *LZSS_unknown_array;
+char *LZSS_circular_buffer;
 
-void FUN_00404a20(char *compressed_data)
+void set_lzss_src(char *compressed_data)
 
 {
   LZSS_compressed_data = compressed_data;
-  DAT_00412fbc = 0;
-  DAT_00412fbd = 0;
+  current_compressed_byte = 0;
+  LZSS_bit_counter = 0;
   return;
 }
 
-void FUN_00404ac0(char *output_memory)
+void set_lzss_dst(char *output_memory)
 
 {
   LZSS_Output_Memory = output_memory;
@@ -38,54 +38,53 @@ void FUN_00404ac0(char *output_memory)
   return;
 }
 
-bool FUN_00404a70(void)
+bool get_next_bit(void)
 
 {
   int uVar1;
 
-  if (DAT_00412fbd == 0) {
-    DAT_00412fbc = *LZSS_compressed_data;
+  if (LZSS_bit_counter == 0) {
+    current_compressed_byte = *LZSS_compressed_data;
     LZSS_compressed_data = LZSS_compressed_data + 1;
   }
-  uVar1 = DAT_00412fbd;
-  DAT_00412fbd += 1;
-  if (DAT_00412fbd == 0x8) {
-    DAT_00412fbd = 0x0;
+  uVar1 = LZSS_bit_counter;
+  LZSS_bit_counter += 1;
+  if (LZSS_bit_counter == 8) {
+    LZSS_bit_counter = 0;
   }
-  return (DAT_0040d098[uVar1] & DAT_00412fbc) != 0;
+  return (BIT_MASKS[uVar1] & current_compressed_byte) != 0;
 }
 
-unsigned int FUN_00404a40(int param_1)
+unsigned int read_bits(int num_bits)
 
 {
   bool bVar1;
-  unsigned int uVar2;
+  unsigned int output_bits;
 
-  uVar2 = 0;
-  if (param_1 != 0) {
+  output_bits = 0;
+  if (num_bits != 0) {
     do {
-      uVar2 <<= 1;
-      bVar1 = FUN_00404a70();
+      output_bits <<= 1;
+      bVar1 = get_next_bit();
       if (bVar1 != 0) {
-        uVar2 |= 1;
+        output_bits |= 1;
       }
-      param_1 += -1;
-    } while (param_1 != 0);
+      num_bits += -1;
+    } while (num_bits != 0);
   }
-  return uVar2;
+  return output_bits;
 }
 
-char *FUN_00404a00(unsigned int param_1)
+char *get_circular_buffer_element_pointer(unsigned int param_1)
 
 {
-  printf("FUN_00404a00 - param_1 = %i, offset = 0x%x\n", param_1, (DAT_00412fd0 - 1 & param_1));
-  return (char *)((DAT_00412fd0 - 1 & param_1) + LZSS_unknown_array);
+  return (char *)((DAT_00412fd0 - 1 & param_1) + LZSS_circular_buffer);
 }
 
-void FUN_00404b60(int param_1, char param_2)
+void write_n_of_byte(unsigned int param_1, unsigned char param_2)
 
 {
-  printf("FUN_00404b60 - param_1=%i, param_2=0x%x\n", param_1, param_2);
+  /* printf("writing %u of %02x\n", param_1, param_2); */
   if (param_1 != 0) {
     do {
       *LZSS_Output_Memory = param_2;
@@ -96,9 +95,10 @@ void FUN_00404b60(int param_1, char param_2)
   return;
 }
 
-void FUN_00404ae0(int param_1)
+void write_output(unsigned int param_1)
 
 {
+    /* printf("write_output %u\n", param_1); */
   if (DAT_00412fe0 == 0) {
     DAT_00412fdc = param_1;
     DAT_00412fe0 = 1;
@@ -115,29 +115,30 @@ void FUN_00404ae0(int param_1)
         return;
       }
     }
-    FUN_00404b60(DAT_00412fe0, (char)param_1);
+    write_n_of_byte(DAT_00412fe0, (char)param_1);
     DAT_00412fe0 = 1;
     return;
   }
   if (param_1 != DAT_00412fdc) {
-    FUN_00404b60(1, (char)param_1);
+    write_n_of_byte(1, (char)param_1);
     return;
   }
   DAT_00412fe0 = 2;
   return;
 }
 
-int FUN_00404950(void)
+int read_displacement(void)
 
 {
   bool bVar1;
   unsigned int uVar2;
   int iVar3;
 
-  uVar2 = FUN_00404a40(DAT_00412fc8 - 1);
+  /* printf("DAT_00412fc8 = %i", DAT_00412fc8); */
+  uVar2 = read_bits(DAT_00412fc8 - 1);
   if (DAT_00412fd4 - 1 <= (int)uVar2) {
     iVar3 = uVar2 * 2;
-    bVar1 = FUN_00404a70();
+    bVar1 = get_next_bit();
     if (bVar1 != 0) {
       iVar3 += 1;
     }
@@ -149,19 +150,19 @@ int FUN_00404950(void)
   return uVar2;
 }
 
-int FUN_00404900(void)
+int read_length(void)
 
 {
   bool bVar1;
   unsigned int uVar2;
   int iVar3;
 
-  uVar2 = FUN_00404a40(DAT_00412fcc - 1);
+  uVar2 = read_bits(DAT_00412fcc - 1);
   if (uVar2 == 1) {
     return 2;
   }
   iVar3 = uVar2 * 2;
-  bVar1 = FUN_00404a70();
+  bVar1 = get_next_bit();
   if (bVar1 != 0) {
     iVar3 += 1;
   }
@@ -174,74 +175,80 @@ int FUN_00404900(void)
   return iVar3;
 }
 
-void FUN_004049a0(int param_1, int param_2)
+void copy_from_buffer(int length, int displacement)
 
 {
-  char *bVar1;
+  unsigned char bVar1;
   unsigned int uVar2;
   char *pbVar3;
 
-  if (param_1 != 0) {
+  if (length != 0) {
     do {
-      pbVar3 = FUN_00404a00(param_2 + DAT_00412fc4);
-      uVar2 = DAT_00412fc4;
-      bVar1 = pbVar3;
-      DAT_00412fc4 += 1;
-      pbVar3 = FUN_00404a00(uVar2);
-      pbVar3 = bVar1;
-      FUN_00404ae0(*bVar1);
-      param_1 += -1;
-    } while (param_1 != 0);
+      pbVar3 = get_circular_buffer_element_pointer(displacement + circular_buffer_current);
+      uVar2 = circular_buffer_current;
+      bVar1 = *pbVar3;
+      circular_buffer_current += 1;
+      pbVar3 = get_circular_buffer_element_pointer(uVar2);
+      *pbVar3 = bVar1;
+      /* printf("copy_from_buffer bVar1 = %02x\n", bVar1); */
+      write_output(bVar1);
+      length -= 1;
+    } while (length != 0);
   }
   return;
 }
 
 void DecompressLZSS(char *compressed_data, char *output_memory,
-                    void *unknown_array)
+                    void *circular_buffer)
 
 {
   unsigned int uVar1;
-  bool bVar2;
+  bool flag;
   unsigned int uVar3;
   char *puVar4;
-  int iVar5;
-  int iVar6;
+  unsigned int disp;
+  unsigned int displacement;
+  unsigned int length;
 
-  FUN_00404a20(compressed_data + 8);
-  FUN_00404ac0(output_memory);
-  DAT_00412fc8 = FUN_00404a40(5);
+  set_lzss_src(compressed_data + 8);
+  set_lzss_dst(output_memory);
+  // usually b01011 (11)
+  DAT_00412fc8 = read_bits(5);
   printf("DAT_00412fc8 = %i\n", DAT_00412fc8);
-  DAT_00412fcc = FUN_00404a40(4);
+  // usually b0100 (4)
+  DAT_00412fcc = read_bits(4);
   printf("DAT_00412fcc = %i\n", DAT_00412fcc);
-  DAT_00412fd0 = 1 << ((unsigned char)DAT_00412fc8 & 0x1f);
+  // number of possible values for a 11 bit integer
+  DAT_00412fd0 = 1 << (DAT_00412fc8 & 0x1f);
   printf("DAT_00412fd0 = %i\n", DAT_00412fd0);
-  LZSS_unknown_array = unknown_array;
-  DAT_00412fc4 = 0;
-  DAT_00412fd4 = 1 << ((unsigned char)DAT_00412fcc & 0x1f);
+  LZSS_circular_buffer = circular_buffer;
+  circular_buffer_current = 0;
+  // number of possible values for a 4 bit integer
+  DAT_00412fd4 = 1 << (DAT_00412fcc & 0x1f);
   printf("DAT_00412fd4 = %i\n", DAT_00412fd4);
   do {
-    bVar2 = FUN_00404a70();
-    if (bVar2 == 0) {
-      iVar5 = FUN_00404950();
-      printf("iVar5 = %i\n", iVar5);
-      if (iVar5 == -1) {
+    flag = get_next_bit();
+    if (flag == 0) {
+      disp = read_displacement();
+      /* printf("disp = %i\n", disp); */
+      if (disp == -1) {
         return;
       }
-      iVar6 = (DAT_00412fd0 - iVar5) + -1;
-      printf("iVar6 = %i\n", iVar6);
-      iVar5 = FUN_00404900();
-      printf("iVar5 = %i\n", iVar5);
-      FUN_004049a0(iVar5, iVar6);
+      displacement = (DAT_00412fd0 - disp) + -1;
+      /* printf("displacement = %i\n", displacement); */
+      length = read_length();
+      /* printf("length = %i\n", length); */
+      copy_from_buffer(length, displacement);
     } else {
-      uVar3 = FUN_00404a40(8);
-      uVar1 = DAT_00412fc4;
-      DAT_00412fc4 += 1;
-      puVar4 = FUN_00404a00(uVar1);
-      printf("puVar4 = %p, uVar3 = 0x%x\n", puVar4, uVar3);
+      uVar3 = read_bits(8);
+      /* printf("main uVar3 = %i\n", uVar3); */
+      uVar1 = circular_buffer_current;
+      circular_buffer_current += 1;
+      puVar4 = get_circular_buffer_element_pointer(uVar1);
       *puVar4 = (char)uVar3;
-      FUN_00404ae0(uVar3);
+      write_output(uVar3);
     }
-    DAT_00412fc4 &= DAT_00412fd0 - 1U;
+    circular_buffer_current &= DAT_00412fd0 - 1U;
   } while (true);
 }
 
@@ -266,7 +273,7 @@ int load_file_to_memory(const char *filename, char **result) {
 }
 
 int main(int argc, char *argv[]) {
-  if (argc != 4) {
+  if (argc != 3) {
     printf("Too many or too few arguments\nUsage: lzss in_filename "
            "out_filename\n");
     return 1;
@@ -291,9 +298,9 @@ int main(int argc, char *argv[]) {
   uncompressed_size = *(uint32_t *)&compressed_data[4];
   printf("uncompressed_size: %d\n", uncompressed_size);
   uncompressed_data = (char *)malloc(uncompressed_size);
-  void *unknown_array = malloc(65536);
+  void *circular_buffer = malloc(65536);
 
-  DecompressLZSS(compressed_data, uncompressed_data, unknown_array);
+  DecompressLZSS(compressed_data, uncompressed_data, circular_buffer);
   FILE *f = fopen(argv[2], "wb");
   if (f == NULL) {
     printf("failed to open output file\n");
@@ -301,12 +308,5 @@ int main(int argc, char *argv[]) {
   }
   fwrite(uncompressed_data, uncompressed_size, 1, f);
   fclose(f);
-  FILE *fp = fopen(argv[3], "wb");
-  if (fp == NULL) {
-    printf("failed to open output file\n");
-    return 1;
-  }
-  fwrite(unknown_array, 65536, 1, fp);
-  fclose(fp);
   return 0;
 }
